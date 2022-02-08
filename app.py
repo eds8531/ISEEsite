d2f243194b1c95d9891e032d15fb60bf725b93b4
eds8531 <eschlosser@gmail.com>
2022-02-08 11:29:51 -0500

    Another commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from myproject import app, db
 from flask import render_template, redirect, request, url_for, flash, abort
 from flask_login import login_user, login_required, logout_user
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
